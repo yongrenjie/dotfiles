329d2110489e8e555913119c7f1c96a10c563726
Jonathan Yong <yongrenjie@gmail.com>
2020-01-26 16:36:21 +0000

    Update vim update script.
    make install on Ubuntu needs sudo. Added note about Ubuntu dependencies.

--- update_vim.sh
+++ update_vim.sh
@@ -5,11 +5,15 @@ make distclean
 if [[ "$OSTYPE" == "darwin"* ]]; then
     # make doesn't know where to find the Homebrew installation, so must be manually specified.
     ./configure --enable-cscope --enable-python3interp --enable-perlinterp --enable-luainterp --with-lua-prefix=/usr/local --enable-rubyinterp --with-ruby-command=/usr/local/opt/ruby/bin/ruby LDFLAGS=-L/usr/local/opt/ruby/lib CPPFLAGS=-I/usr/local/opt/ruby/include
+    make -j
+    sudo make install
 fi
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    # requires sudo apt-get install lua5.2 liblua5.2-dev perl libperl-dev python3.8-dev
+    # amongst others... but for now everything is fine
     ./configure --enable-cscope --enable-python3interp --enable-perlinterp --enable-luainterp --enable-rubyinterp
+    make -j
+    sudo make install
 fi
-make -j
-make install
 cd $curdir
 unset curdir
