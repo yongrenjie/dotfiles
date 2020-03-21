a48748a99453cbbf916d3346c1767a64ede89056
Jonathan Yong <yongrenjie@gmail.com>
2020-03-21 14:06:32 +0800

    Script to make necessary symlinks

---
+++ make_symlinks.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+files=(".vimrc" \
+    ".vim" \
+    ".bash_profile" \
+    ".dircolors" \
+    ".git-completion.bash" \
+    ".tmux.conf" \
+    ".bashrc" \
+)
+DOTFILES="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+for f in ${files[@]}; do
+    if [[ -L ${HOME}/${f} ]] || [[ ! -a ${HOME}/${f} ]] ; then
+        rm ${HOME}/${f} 2&>/dev/null
+        ln -s ${DOTFILES}/${f} ${HOME}/${f}
+    else
+        printf "non-symlink file was detected:   %s\n" ${f}
+    fi
+done
